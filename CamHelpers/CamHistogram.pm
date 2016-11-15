d9e9eedf8097ed24d129123af81ec74bcfa45d75
stepan <stepan.prichystal@gatema.cz>
2016-11-15 15:27:25 +0100

    Drill checking

---
+++ CamHelpers/CamHistogram.pm
@@ -0,0 +1,131 @@
+#-------------------------------------------------------------------------------------------#
+# Description: Helper class, contains general function working with InCAM histogram (features, attributes etc)
+# Author:SPR
+#-------------------------------------------------------------------------------------------#
+
+package CamHelpers::CamHistogram;
+
+#3th party library
+use strict;
+use warnings;
+
+#loading of locale modules
+
+#use aliased 'Enums::EnumsPaths';
+#use aliased 'CamHelpers::CamHelper';
+#use aliased 'CamHelpers::CamJob';
+#use aliased 'CamHelpers::CamCopperArea';
+#use aliased 'Connectors::HeliosConnector::HegMethods';
+#use aliased 'Helpers::GeneralHelper';
+
+#-------------------------------------------------------------------------------------------#
+#   Package methods
+#-------------------------------------------------------------------------------------------#
+
+# Return name of all steps 'FEATURES',
+sub GetAttHistogram {
+	my $self      = shift;
+	my $inCAM     = shift;
+	my $jobId     = shift;
+	my $stepName  = shift;
+	my $layerName = shift;
+	my $breakSR   = shift;
+	my $selected   = shift;
+
+	my $sr = "break_sr+";
+	if ( defined $breakSR && $breakSR == 0 ) {
+		$sr = "";
+	}
+	
+	 my $sel = "";
+	if ( $selected) {
+		$sel = "select+";
+	}
+
+	my $fFeatures = $inCAM->INFO(
+								  units           => 'mm',
+								  angle_direction => 'ccw',
+								  entity_type     => 'layer',
+								  entity_path     => "$jobId/$stepName/$layerName",
+								  data_type       => 'FEATURES',
+								  options         => $sel.$sr . "f0",
+								  parse           => 'no'
+	);
+
+	my %attHist = ();
+
+	my $f;
+	open( $f, $fFeatures );
+
+	while ( my $l = <$f> ) {
+
+		if ( $l =~ /###/ ) { next; }
+
+		$l =~ m/.*;(.*)/;
+
+		unless ($1) {
+			next;
+		}
+
+		my @attr = split( ",", $1 );
+
+		foreach my $at (@attr) {
+
+			my @parse = split( "=", $at );
+
+			unless ( $attHist{ $parse[0] } ) {
+				my @attVal = ();
+				$attHist{ $parse[0] } = \@attVal;
+			}
+
+			if ( defined $parse[1] ) {
+
+				# check if same attribute value  exist
+				my $exist = scalar( grep { $_ eq $parse[1] } @{ $attHist{ $parse[0] } } );
+				unless ($exist) {
+					push( @{ $attHist{ $parse[0] } }, $parse[1] );
+				}
+
+			}
+
+		}
+	}
+	return %attHist;
+}
+
+# Return name of all steps
+sub GetFeatuesHistogram {
+	my $self      = shift;
+	my $inCAM     = shift;
+	my $jobId     = shift;
+	my $stepName  = shift;
+	my $layerName = shift;
+	my $breakSR   = shift;
+
+	my $sr = "break_sr";
+	if ( defined $breakSR && $breakSR == 0 ) {
+		$sr = "";
+	}
+
+	$inCAM->INFO(
+				  units             => 'mm',
+				  "angle_direction" => 'ccw',
+				  "entity_type"     => 'layer',
+				  "entity_path"     => "$jobId/$stepName/$layerName",
+				  "data_type"       => 'FEAT_HIST',
+				  "parameters"      => "arc+line+pad+surf+text+total",
+				  "options"         => $sr
+	);
+
+	my %info = ();
+
+	$info{"pad"}   = $inCAM->{doinfo}{gFEAT_HISTpad};
+	$info{"surf"}  = $inCAM->{doinfo}{gFEAT_HISTsurf};
+	$info{"arc"}   = $inCAM->{doinfo}{gFEAT_HISTarc};
+	$info{"text"}  = $inCAM->{doinfo}{gFEAT_HISTtext};
+	$info{"total"} = $inCAM->{doinfo}{gFEAT_HISTtotal};
+
+	return %info;
+}
+
+1;
--- Packages/Drilling/DrillChecking/LayerCheck.pm
+++ Packages/Drilling/DrillChecking/LayerCheck.pm
@@ -5,61 +5,432 @@
 package Packages::Drilling::DrillChecking::LayerCheck;
 
 #3th party library
+use List::MoreUtils qw(uniq);
 
 #local library
 
-use aliased 'Helpers::GeneralHelper';
+#use aliased 'Helpers::GeneralHelper';
 use aliased 'Enums::EnumsGeneral';
-use aliased 'CamHelpers::CamHelper';
-use aliased 'Helpers::FileHelper';
+
+#use aliased 'CamHelpers::CamHelper';
+#use aliased 'Helpers::FileHelper';
+use aliased 'CamHelpers::CamToolDepth';
+use aliased 'CamHelpers::CamDrilling';
+use aliased 'CamHelpers::CamJob';
+use aliased 'CamHelpers::CamHistogram';
+
 #-------------------------------------------------------------------------------------------#
 #  Script methods
 #-------------------------------------------------------------------------------------------#
 
+sub CheckNCLayers {
+	my $self  = shift;
+	my $inCAM = shift;
+	my $jobId = shift;
+	my $mess  = shift;
 
- sub CheckWrongNames{
-  	my $self = shift;
- 	my $mess = shift; 
- 	
- 	my $succ = 0;
- 	
- 	
- 	
- 	
- }
+	my $result = 1;
 
+	# Get all layers
+	my @layers = ( CamJob->GetLayerByType( $inCAM, $jobId, "drill" ), CamJob->GetLayerByType( $inCAM, $jobId, "rout" ) );
 
- sub CheckBlindDrill{
- 	my $self = shift;
- 	my $inCAM = shift;
- 	my $jobId = shift;
- 	
- 	my $mess = shift; 
- 		
-  	my @drillLayers = @{shift(@_)}; 	
- 	
- 	my $succ = 0;
- 	
- 	@layers  = grep { $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillTop ||  $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillBot } @drillLayers;
- 	
- 	foreach my $l (@layers){
- 		
- 		 
- 		
- 		#get depths for all diameter
-		my @toolDepths = $self->GetToolDepths( $inCAM, $jobId, "panel", $_->{"gROWname"} );
-		@toolDepths
- 		
- 		
- 		
- 	}
- 	
- 	
- 	
- }
+	CamDrilling->AddNCLayerType( \@layers );
+	CamDrilling->AddLayerStartStop( $inCAM, $jobId, \@layers );
+
+	# Add histogram
+
+	foreach my $l (@layers) {
+
+		my %fHist = CamHistogram->GetFeatuesHistogram( $inCAM, $jobId, "panel", $l->{"gROWname"} );
+		$l->{"fHist"} = \%fHist;
+
+		my %attHist = CamHistogram->GetAttHistogram( $inCAM, $jobId, "panel", $l->{"gROWname"} );
+		$l->{"attHist"} = \%attHist;
+	}
+
+	# 1) Check if some layer has wronng name
+
+	unless ( $self->CheckWrongNames( \@layers, $mess ) ) {
+
+		$result = 0;
+	}
+
+	# 2) Check if layer is not empty
+
+	unless ( $self->CheckIsNotEmpty( \@layers, $mess ) ) {
+
+		$result = 0;
+	}
+
+	# 3) Check if layer not contain attribute nomenclature
+
+	unless ( $self->CheckAttributes( \@layers, $mess ) ) {
+
+		$result = 0;
+	}
+
+	# 4) check each NC layers bz type
+
+	# Blind
+	unless ( $self->CheckBlindDrill( $inCAM, $jobId, \@layers, $mess ) ) {
+
+		$result = 0;
+	}
+
+	# standard drill
+	unless ( $self->CheckDrill( $inCAM, $jobId, \@layers, $mess ) ) {
+
+		$result = 0;
+	}
+
+	# core drill
+	unless ( $self->CheckCoreDrill( $inCAM, $jobId, \@layers, $mess ) ) {
+
+		$result = 0;
+	}
+
+	# Depth rout plated/nplated
+	unless ( $self->CheckBlindDrill( $inCAM, $jobId, \@layers, $mess ) ) {
+
+		$result = 0;
+	}
+
+	return $result;
+
+}
+
+sub CheckIsNotEmpty {
+	my $self   = shift;
+	my @layers = @{ shift(@_) };
+	my $mess   = shift;
+
+	my $result = 1;
+
+	foreach my $l (@layers) {
+
+		if ( $l->{"fHist"}->{"total"} == 0 ) {
+			$result = 0;
+			$$mess .= "NC layer: " . $l->{"gROWname"} . " is empty.\n";
+		}
+	}
+
+	return $result;
+}
+
+sub CheckAttributes {
+	my $self   = shift;
+	my @layers = @{ shift(@_) };
+	my $mess   = shift;
+
+	my $result = 1;
+
+	foreach my $l (@layers) {
+
+		if ( $l->{"attHist"}->{".nomenclature"} ) {
+			$result = 0;
+			$$mess .= "NC layer: " . $l->{"gROWname"} . " contains attribut .nomenclature.\n";
+		}
+	}
+
+	return $result;
+}
+
+sub CheckWrongNames {
+	my $self   = shift;
+	my @layers = @{ shift(@_) };
+	my $mess   = shift;
+
+	my $result = 1;
+
+	foreach my $l (@layers) {
+
+		unless ( $l->{"type"} ) {
+			$result = 0;
+			$$mess .= "NC layer: " . $l->{"gROWname"} . " has wrong name.\n";
+		}
+	}
+
+	return $result;
+}
+
+sub CheckBlindDrill {
+	my $self   = shift;
+	my $inCAM  = shift;
+	my $jobId  = shift;
+	my @layers = @{ shift(@_) };
+	my $mess   = shift;
+
+	my $result = 1;
+
+	@layers = grep { $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillTop || $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillBot } @layers;
+
+	# 1) check right direction of drill
+
+	foreach my $l (@layers) {
+
+		my $dir   = $l->{"gROWdrl_dir"};
+		my $lName = $l->{"gROWname"};
+
+		if ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillTop ) {
+
+			if ( $dir && $dir ne "top2bot" ) {
+				$result = 0;
+				$$mess .= "Layer $lName has wrong direction of drilling. Direction has to be: top2bot. \n";
+			}
+
+		}
+		elsif ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillBot ) {
+
+			if ( $dir ne "bot2top" ) {
+				$result = 0;
+				$$mess .= "Layer $lName has wrong direction of drilling. . Direction has to be: bot2top. \n";
+			}
+		}
+	}
+
+	# 2) check start and end layer
+
+	foreach my $l (@layers) {
+		my $startL    = $l->{"gROWdrl_start"};
+		my $endL      = $l->{"gROWdrl_end"};
+		my $layerName = $l->{"gROWname"};
+
+		if ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillTop ) {
+
+			if ( $startL >= $endL ) {
+				$result = 0;
+				$$mess .= "Layer: $layerName, drilling start/end layer is wrong in matrix.\n";
+			}
+
+		}
+		elsif ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillBot ) {
+
+			if ( $endL <= $StartL ) {
+				$result = 0;
+				$$mess .= "Layer: $layerName, drilling start/end layer is wrong in matrix.\n";
+			}
+		}
+	}
+
+	# 3) check if tool depth is set
+
+	foreach my $l (@layers) {
+
+		$self->__ToolDepthSet( $inCAM, $jobId, $l->{"gROWname"}, $mess );
+	}
+}
+
+sub CheckDrill {
+	my $self   = shift;
+	my $inCAM  = shift;
+	my $jobId  = shift;
+	my @layers = @{ shift(@_) };
+	my $mess   = shift;
+
+	my $result = 1;
+
+	@layers = grep { $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_nDrill || $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_cDrill } @layers;
+
+	foreach my $l (@layers) {
+
+		# 1) check right direction of drill
+
+		my $dir   = $l->{"gROWdrl_dir"};
+		my $lName = $l->{"gROWname"};
+
+		if ( $dir && $dir ne "top2bot" ) {
+			$result = 0;
+			$$mess .= "Layer $lName has wrong direction of routing. Direction has to be: top2bot. \n";
+		}
+
+		# 2) check start and end layer
+
+		my $startL = $l->{"gROWdrl_start"};
+		my $endL   = $l->{"gROWdrl_end"};
+
+		# normal drill
+		if ( $lName =~ /^m$/ ) {
+			if ( $startL >= $endL ) {
+				$result = 0;
+				$$mess .= "Layer: $lName, drill start/end layer is wrong in matrix.\n";
+			}
+		}
+
+		# blind - through drill
+		elsif ( $lName =~ /^m\d+$/ ) {
+
+			my $lCnt = CamJob->GetSignalLayerCnt( $inCAM, $jobId );
+
+			if ( $startL >= $endL || $startL == 1 || $endL == $lCnt ) {
+				$result = 0;
+				$$mess .= "Layer: $lName, (blind-through drilling) start/end layer is wrong in matrix.\n";
+			}
+			
+		}
+		
+		# core drill
+		elsif ( $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_cDrill ) {
+
+			if ( abs($startL - $endL) != 1 ) {
+				$result = 0;
+				$$mess .= "Layer: $lName, start/end layer is wrong in matrix. Only core layer could be drilled.\n";
+			}
+		}
+		
+	}
+	
+	
+	# check if there is no tool depth in layer blind-through
+	foreach my $l (@layers) {
+		
+		my $lName = $l->{"gROWname"};
+		
+		if ( $lName =~ /^m\d+$/ ) {
+			
+			# there are tool depths
+			if($self->__ToolDepthSet( $inCAM, $jobId, $l->{"gROWname"}, $mess )){
+				
+				
+			}
+			
+			
+		}
+		
+		
  
- 
- 
+	}
+	
+	
+	
+}
+
+sub CheckDepthRout {
+	my $self   = shift;
+	my $inCAM  = shift;
+	my $jobId  = shift;
+	my @layers = @{ shift(@_) };
+	my $mess   = shift;
+
+	my $result = 1;
+
+	@layers = grep {
+		     $_->{"type"} eq EnumsGeneral->LAYERTYPE_nplt_bMillTop
+		  || $_->{"type"} eq EnumsGeneral->LAYERTYPE_nplt_bMillBot
+		  || $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bMillTop
+		  || $_->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bMillBot
+	} @layers;
+
+	# 1) check right direction of drill
+
+	foreach my $l (@layers) {
+
+		my $dir   = $l->{"gROWdrl_dir"};
+		my $lName = $l->{"gROWname"};
+
+		if ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillTop || $l->{"type"} eq EnumsGeneral->LAYERTYPE_nplt_bDrillTop ) {
+
+			if ( $dir && $dir ne "top2bot" ) {
+				$result = 0;
+				$$mess .= "Layer $lName has wrong direction of routing. Direction has to be: top2bot. \n ";
+			}
+
+		}
+		elsif ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillBot || $l->{"type"} eq EnumsGeneral->LAYERTYPE_nplt_bDrillBot ) {
+
+			if ( $dir ne "bot2top" ) {
+				$result = 0;
+				$$mess .= "Layer $lName has wrong direction of routing. . Direction has to be: bot2top. \n ";
+			}
+		}
+	}
+
+	# 2) check start and end layer
+
+	foreach my $l (@layers) {
+		my $startL    = $l->{"gROWdrl_start"};
+		my $endL      = $l->{"gROWdrl_end"};
+		my $layerName = $l->{"gROWname"};
+
+		if ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillTop || $l->{"type"} eq EnumsGeneral->LAYERTYPE_nplt_bDrillTop ) {
+
+			if ( $startL >= $endL ) {
+				$result = 0;
+				$$mess .= "Layer: $layerName, routing start/end layer is wrong in matrix.\n ";
+			}
+
+		}
+		elsif ( $l->{"type"} eq EnumsGeneral->LAYERTYPE_plt_bDrillBot || $l->{"type"} eq EnumsGeneral->LAYERTYPE_nplt_bDrillBot ) {
+
+			if ( $endL <= $StartL ) {
+				$result = 0;
+				$$mess .= "Layer: $layerName, routing start/end layer is wrong in matrix.\n ";
+			}
+		}
+	}
+
+	# 3) check if tool depth is set
+
+	foreach my $l (@layers) {
+
+		$self->__ToolDepthSet( $inCAM, $jobId, $l->{"gROWname"}, $mess );
+	}
+}
+
+sub __ToolDepthSet {
+	my $self      = shift;
+	my $inCAM     = shift;
+	my $jobId     = shift;
+	my $layerName = shift;
+	my $mess      = shift;
+
+	my $stepName = "panel";
+
+	my $result = 1;
+
+	#get depths for all diameter
+	my @toolDepths = CamToolDepth->GetToolDepths( $inCAM, $jobId, $stepName, $layerName );
+
+	$inCAM->INFO(
+				  units       => 'mm',
+				  entity_type => 'layer',
+				  entity_path => "$jobId/$stepName/$layerName",
+				  data_type   => 'TOOL',
+				  parameters  => 'drill_size+shape',
+				  options     => "break_sr"
+	);
+	my @toolSize  = @{ $inCAM->{doinfo}{gTOOLdrill_size} };
+	my @toolShape = @{ $inCAM->{doinfo}{gTOOLshape} };
+
+	# 1) Check if there are same tools
+	my @uniq = uniq @toolSize;
+
+	if ( scalar(@uniq) < scalar(@toolSize) ) {
+
+		$result = 0;
+		$$mess .= "Layer: $layerName, contain more tools with same tool-size. Tool size has to be unique in Drill tool table. \n ";
+	}
+
+	# 2) check if tool depth is set
+	for ( my $i = 0 ; $i < scalar(@toolSize) ; $i++ ) {
+
+		my $tSize = $toolSize[$i];
+
+		#for each hole diameter, get depth (in mm)
+		my $tDepth;
+
+		my $prepareOk = CamToolDepth->PrepareToolDepth( $tSize, \@toolDepths, \$tDepth );
+		unless ($prepareOk) {
+
+			$result = 0;
+			$$mess .= "Layer: $layerName, depth is not valid/set for tool: $tSize mm.\n ";
+
+		}
+	}
+
+	return $result;
+
+}
+
 # Function return max aspect ratio from all holes and their depths. For given layer
 sub GetMaxAspectRatioByLayer {
 	my $self      = shift;
@@ -112,8 +483,6 @@ sub GetMaxAspectRatioByLayer {
 	return $aspectRatio;
 }
 
-
-
 #-------------------------------------------------------------------------------------------#
 #  Place for testing..
 #-------------------------------------------------------------------------------------------#
@@ -121,10 +490,20 @@ sub GetMaxAspectRatioByLayer {
 my ( $package, $filename, $line ) = caller;
 if ( $filename =~ /DEBUG_FILE.pl/ ) {
 
-	#my $test = StackupLayerHelper->GetStackupPress("F14742");
+	use aliased 'Packages::Drilling::DrillChecking::LayerCheck';
 
- 
-	#print $test;
+	use aliased 'Packages::InCAM::InCAM';
+
+	my $inCAM = InCAM->new();
+	my $jobId = "f52456";
+
+	my $mess = "";
+
+	my $result = LayerCheck->CheckNCLayers( $inCAM, $jobId, \$mess );
+
+	print STDERR "Result is $result \n";
+
+	print STDERR " $mess \n";
 
 }
 
