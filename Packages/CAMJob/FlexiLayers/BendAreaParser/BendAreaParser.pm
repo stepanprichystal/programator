42b39d05fa4e2669ba30d5ed85aa6fbf4b09813d
stepan <stepan.prichystal@gatema.cz>
2019-03-07 13:34:26 +0100

    Change branch
    Former-commit-id: 8b7a0e6cfc3f846b27e48150627f65558b1f5b5b

---
+++ Packages/CAMJob/FlexiLayers/BendAreaParser/BendAreaParser.pm
@@ -0,0 +1,196 @@
+
+#-------------------------------------------------------------------------------------------#
+# Description: Parse "bend area" layer for rigid flex pcb
+# - BendArea class contain closed polygon features, which has always CCW direction
+# - Features in "BendArea" not match with original layer features. (feat id can
+#   be different, arcs can be aproxiamted)
+# - Transition zone feature is oriented as well CCW, thus pcb rigid part of
+#   is always on right
+# Author:SPR
+#-------------------------------------------------------------------------------------------#
+package Packages::CAMJob::FlexiLayers::BendAreaParser::BendAreaParser;
+
+#3th party library
+use strict;
+use warnings;
+
+#local library
+use aliased 'Packages::Polygon::Features::PolyLineFeatures::PolyLineFeatures';
+use aliased 'Packages::CAMJob::FlexiLayers::BendAreaParser::BendArea';
+use aliased 'Packages::CAMJob::FlexiLayers::BendAreaParser::TransitionZone';
+use aliased 'Packages::Polygon::Polygon::PolygonPoints';
+use aliased 'Packages::Polygon::Enums' => 'PolyEnums';
+
+#-------------------------------------------------------------------------------------------#
+#  Public method
+#-------------------------------------------------------------------------------------------#
+
+my $TRANSITIONZONEATT = "transition_zone";
+
+sub new {
+	my $self = shift;
+	$self = {};
+	bless $self;
+
+	$self->{"inCAM"} = shift;
+	$self->{"jobId"} = shift;
+	$self->{"step"}  = shift;
+	$self->{"layer"} = shift // "bend";
+
+	# PROPERTIES
+
+	$self->{"bendAreas"} = [];
+	
+	$self->__LoadBendArea();
+
+	return $self;
+}
+
+
+
+sub CheckBendArea {
+	my $self    = shift;
+	my $errMess = shift;
+
+	my $result = 1;
+
+	# 1) Check if polygons have two transition zone
+	foreach my $bendArea ( @{ $self->{"bendAreas"} } ) {
+
+		# 1) Check minimal number of bend area features (4)
+		my @features = $bendArea->GetFeatures();
+
+		if ( scalar(@features) < 4 ) {
+
+			$errMess .= "BendArea with features: " . join( "; ", $bendArea->GetFeatures() ) . "\n" . $errMess .=
+			  "Number of bend area features has to be at least four (current number is: " . scalar(@features) . ")\n";
+			$result = 0;
+		}
+
+		# 2) Check requested number of transition zone (2)
+		my @tZones = $bendArea->GetTransitionZones();
+
+		if ( scalar(@tZones) != 2 ) {
+
+			$errMess .= "BendArea with features: " . join( "; ", $bendArea->GetFeatures() ) . "\n" . $errMess .=
+			  "Number of transition zone is not: 2 (current number is: " . scalar(@tZones) . ")\n";
+			$result = 0;
+		}
+
+		# 3) Check if transition yones contain only lines
+		 
+		foreach my $tZone (@tZones) {
+
+			if ( $tZone->GetFeature()->{"type"} ne "L" ) {
+
+				$errMess .= "Only line features are allowed for transition zone (err feat id: " . $tZone->GetFeature()->{"id"} . ")\n";
+				$result = 0;
+			}
+		}
+
+	}
+	
+	return $result;
+}
+
+sub GetBendAreas{
+	my $self    = shift;
+	
+	return @{$self->{"bendAreas"}};
+}
+
+
+sub __LoadBendArea {
+	my $self = shift;
+
+	my $inCAM = $self->{"inCAM"};
+	my $jobId = $self->{"jobId"};
+	my $step  = $self->{"step"};
+	my $layer = $self->{"layer"};
+
+	my $polyLine = PolyLineFeatures->new();
+	$polyLine->Parse( $inCAM, $jobId, $step, $layer );
+
+	# return parsed feature polygons, cyclic only CW or CCW)
+	my @polygons = $polyLine->GetPolygonsFeatures();
+
+	# 1) Check if there is detected some polygon area
+	die "No bend area detected in step: $step; layer: $layer" if ( !scalar(@polygons) );
+
+	foreach my $polygon (@polygons) {
+
+		my @tranZones = ();
+
+		foreach my $feat ( grep { defined $_->{"att"}->{$TRANSITIONZONEATT} } @{$polygon} ) {
+
+			my $tZone = TransitionZone->new($feat);
+
+			push( @tranZones, $tZone );
+
+		}
+
+		# switch direction to CWW
+		my @points = map { [ $_->{"x1"}, $_->{"y1"} ] } @{$polygon};    # rest of points "x2,y2"
+		my $dir = PolygonPoints->GetPolygonDirection( \@points );
+
+		my @feats = @{$polygon};
+
+		if ( $dir ne PolyEnums->Dir_CCW ) {
+
+			@feats = reverse(@feats);
+
+			foreach my $f (@feats) {
+
+				my $pX = $f->{"x2"};
+				my $pY = $f->{"y2"};
+				$f->{"x2"} = $f->{"x1"};
+				$f->{"y2"} = $f->{"y1"};
+				$f->{"x1"} = $pX;
+				$f->{"y1"} = $pY;
+
+				# switch direction
+				if ( $f->{"type"} eq "A" ) {
+
+					$f->{"newDir"} = $f->{"oriDir"} eq EnumsRout->Dir_CW ? EnumsRout->Dir_CCW : EnumsRout->Dir_CW;
+				}
+			}
+		}
+
+		my $bendArea = BendArea->new( \@feats, \@tranZones );
+
+		push( @{ $self->{"bendAreas"} }, $bendArea );
+
+	}
+
+}
+
+#-------------------------------------------------------------------------------------------#
+#  Place for testing..
+#-------------------------------------------------------------------------------------------#
+my ( $package, $filename, $line ) = caller;
+if ( $filename =~ /DEBUG_FILE.pl/ ) {
+
+	use aliased 'Packages::CAMJob::FlexiLayers::BendAreaParser::BendAreaParser';
+	use aliased 'Packages::InCAM::InCAM';
+
+	my $jobId = "d113609";
+	my $inCAM = InCAM->new();
+
+	my $step  = "o+1";
+	my $layer = "s";
+
+	my $parser = BendAreaParser->new( $inCAM, $jobId, $step, );
+
+	$parser->LoadBendArea();
+
+	my $errMess = "";
+
+	unless ( $parser->CheckBendArea(\$errMess) ) {
+
+		print $errMess;
+
+	}
+}
+
+1;
+
--- Packages/Polygon/Features/PolyLineFeatures/PolyLineFeatures.pm
+++ Packages/Polygon/Features/PolyLineFeatures/PolyLineFeatures.pm
@@ -60,18 +60,11 @@ sub GetFeatures {
 	return @{ $self->{"features"} };
 }
 
-sub __GetLineFeatures {
+ 
+sub GetPolygonsFeatures {
 	my $self = shift;
 
-	my @lines = grep { $_->{"type"} eq "L" } @{ $self->{"features"} };
-
-	return @lines;
-}
-
-sub GetPolygonsPoints {
-	my $self = shift;
-
-	my @lines     = $self->__GetLineFeatures();
+	my @lines     = grep { $_->{"type"} eq "L"  ||  $_->{"type"} eq "A"} @{ $self->{"features"} };
 	my @sequences = RoutCyclic->GetRoutSequences( \@lines );
 
 	my @polygons = ();
@@ -85,10 +78,17 @@ sub GetPolygonsPoints {
 		}
 		else {
 
-			die "Plated rout outline is not cyclic";
+			die "Polygon features are not cyclic";
 		}
+	} 
+	
+	return @polygons;
+}
+ 
+sub GetPolygonsPoints {
+	my $self = shift;
 
-	}
+	my @polygons = $self->GetPolygonsFeatures();
 
 	my @polygonsPoints = ();
 
@@ -118,140 +118,43 @@ sub GetPolygonsPoints {
 	return @polygonsPoints;
 
 }
-
-sub GetClosedPolyLines {
-	my $self = shift;
-
-	my @lines = $self->__GetLineFeatures();
-
-	my $x;
-	my $y;
-	my @polygons = ();
-
-	# until we process all lines
-	while ( scalar(@lines) ) {
-
-		my @polygon = ();
-
-		#take arbitrary line
-		my %actLine = %{ $lines[0] };
-		$x = sprintf( "%.2f", $actLine{"x2"} );
-		$y = sprintf( "%.2f", $actLine{"y2"} );
-
-		# save start point
-		push( @polygon, \%actLine );
-		splice @lines, 0, 1;    # dekete from list
-
-		my $polygonFound = 0;
-
-		while ( !$polygonFound ) {
-
-			my $isFind = 0;
-
-			#find next part of chain. Go through all lines
-			for ( my $i = 0 ; $i < scalar(@lines) ; $i++ ) {
-
-				my %searchL = %{ $lines[$i] };
-				$isFind = 0;
-
-				# case when we find next polygon line
-				if (    ( $x == sprintf( "%.2f", $searchL{"x1"} ) && $y == sprintf( "%.2f", $searchL{"y1"} ) )
-					 || ( $x == sprintf( "%.2f", $searchL{"x2"} ) && $y == sprintf( "%.2f", $searchL{"y2"} ) ) )
-				{
-					$isFind = 1;
-
-					#switch edge points for achieve same-oriented polygon
-					if ( ( $x == sprintf( "%.3f", $searchL{"x2"} ) && $y == sprintf( "%.3f", $searchL{"y2"} ) ) ) {
-						my $pX = $searchL{"x2"};
-						my $pY = $searchL{"y2"};
-						$searchL{"x2"} = $searchL{"x1"};
-						$searchL{"y2"} = $searchL{"y1"};
-						$searchL{"x1"} = $pX;
-						$searchL{"y1"} = $pY;
-
-						$searchL{"switchPoints"} = 1;
-					}
-					else {
-
-						$searchL{"switchPoints"} = 0;
-					}
-
-					push( @polygon, \%searchL );
-
-					#%actLine = %searchL;
-
-					$x = sprintf( "%.2f", $searchL{"x2"} );
-					$y = sprintf( "%.2f", $searchL{"y2"} );
-
-					splice @lines, $i, 1;
-
-					# go to find next line
-					last;
-
-				}
-			}
-
-			if ( $isFind == 0 ) {
-
-				$polygonFound = 1;
-
-				# if polygon is closed, save
-				if ( scalar(@polygon) > 1 ) {
-					my $firstLine = $polygon[0];
-					my $lastLine  = $polygon[ scalar(@polygon) - 1 ];
-
-					if ( $lastLine->{"x2"} == $firstLine->{"x1"} && $lastLine->{"y2"} == $firstLine->{"y1"} ) {
-
-						# save searched polygon and search next
-						push( @polygons, \@polygon );
-
-					}
-
-				}
-			}
-
-		}
-
-	}
-
-	return @polygons;
-}
+ 
 
 #-------------------------------------------------------------------------------------------#
 #  Place for testing..
 #-------------------------------------------------------------------------------------------#
 my ( $package, $filename, $line ) = caller;
 if ( $filename =~ /DEBUG_FILE.pl/ ) {
-
-	use aliased 'Packages::Polygon::Features::PolyLineFeatures::PolyLineFeatures';
-	use aliased 'Packages::InCAM::InCAM';
-
-	my $route = PolyLineFeatures->new();
-
-	my $jobId = "f13609";
-	my $inCAM = InCAM->new();
-
-	my $step  = "panel";
-	my $layer = "test";
-
-	$route->Parse( $inCAM, $jobId, $step, $layer );
-
-	my @features = $route->GetFeatures();
-	my @chains   = $route->GetClosedPolyLines();
-	my @points   = $route->GetPolygonsPoints();
-
-	use Math::Polygon;
-
-	my @areas = ();
-
-	foreach my $p (@points) {
-
-		my $p    = Math::Polygon->new( @{$p} );
-		my $area = $p->area;
-
-		push( @areas, $area );
-
-	}
+#
+#	use aliased 'Packages::Polygon::Features::PolyLineFeatures::PolyLineFeatures';
+#	use aliased 'Packages::InCAM::InCAM';
+#
+#	my $route = PolyLineFeatures->new();
+#
+#	my $jobId = "f13609";
+#	my $inCAM = InCAM->new();
+#
+#	my $step  = "panel";
+#	my $layer = "test";
+#
+#	$route->Parse( $inCAM, $jobId, $step, $layer );
+#
+#	my @features = $route->GetFeatures();
+#	my @chains   = $route->GetClosedPolyLines();
+#	my @points   = $route->GetPolygonsPoints();
+#
+#	use Math::Polygon;
+#
+#	my @areas = ();
+#
+#	foreach my $p (@points) {
+#
+#		my $p    = Math::Polygon->new( @{$p} );
+#		my $area = $p->area;
+#
+#		push( @areas, $area );
+#
+#	}
 
 	#print 1;
 
